9764d4924539e023213759f8ec2dfcc646725e31
Simona Ruoss <simonaruoss@Simonas-MacBook-Air.local>
2024-12-16 15:31:43 +0100

    safety added

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,3 +1,32 @@
 public class SudokuSolver {
-    
+
+    public static boolean isSafe(int[][]board, int row, int col, int num){
+        
+        //check column
+        for (int a = 0; a < 9; a++){
+            if (board [a][col] == num){
+                return false;
+            }
+        }
+
+        //check row
+        for (int b = 0; b < 9; b++){
+            if (board [row][b] == num){
+                return false;
+            } 
+        }
+
+        //check square
+        int startRow = row - row%3;
+        int startCol = col - col%3;
+        for (int a = startRow; a < startRow + 3; a++){
+            for (int b = startCol; b < startCol + 3; b ++){
+                if (board[a][b] == num){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
--- Testcode.java
+++ Testcode.java
@@ -1,5 +1,15 @@
 public class Testcode {
     public static void main(String[] args) {
-        
+        //safety
+        int sudokuEasy [][] = { {0,4,0,0,0,7,0,0,0},
+                                {0,0,1,8,9,4,0,0,0},
+                                {8,2,7,0,6,3,0,9,0},
+                                {0,0,0,7,0,8,0,0,4},
+                                {0,8,5,0,4,9,6,0,7},
+                                {0,7,0,6,5,1,2,8,0},
+                                {0,9,6,3,8,2,5,0,0},
+                                {7,0,0,9,1,0,0,0,0},
+                                {0,0,0,0,0,5,0,0,3}};
+        System.out.println(SudokuSolver.isSafe(sudokuEasy, 8, 6, 6));
     }
 }
