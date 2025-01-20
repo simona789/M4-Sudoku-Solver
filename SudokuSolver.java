69496c16ac6e8a217964de53fe5b9b1d559d960a
Simona Ruoss <simonaruoss@simonas-air.lan>
2025-01-20 14:32:10 +0100

    working sudoku solver

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,75 +1,132 @@
 public class SudokuSolver {
 
-    public static boolean isSafe(int[][]board, int row, int col, int num){
-        
+    private static final int GRID_SIZE = 9;
 
-        //check column
-        for (int a = 0; a < 9; a++){
-            if (board [a][col] == num){
+    //check row
+    private static boolean isRowSafe(int[][] board, int row, int number){
+        for (int i = 0; i < GRID_SIZE; i++){
+            if (board[row][i] == number) {
                 return false;
             }
         }
+        return true;
+    }
 
-        //check row
-        for (int b = 0; b < 9; b++){
-            if (board [row][b] == num){
+    //check column
+    private static boolean isColumnSafe(int[][] board, int column, int number){
+        for (int i = 0; i < GRID_SIZE; i++){
+            if (board[i][column] == number) {
                 return false;
-            } 
+            }
         }
+        return true;
+    }
 
-        //check square
-        int startRow = row - row%3;
-        int startCol = col - col%3;
-        for (int a = startRow; a < startRow + 3; a++){
-            for (int b = startCol; b < startCol + 3; b ++){
-                if (board[a][b] == num){
+    //check square
+    private static boolean isSquareSafe(int[][] board, int row, int column, int number){
+        //get top left box of square
+        int localSquareRow = row - row % 3;
+        int localSquareColumn = column - column % 3;
+        //go through rows
+        for (int i = localSquareRow; i < localSquareRow + 3; i ++){
+            //go through columns
+            for (int j = localSquareColumn; j < localSquareColumn + 3; j++){
+                if (board [i][j] == number){
                     return false;
                 }
             }
-        } 
+        }
         return true;
-
     }
 
-    public static boolean solve(int[][]board, int n) {
-        int row = -1;
-        int col = -1;
-        boolean isEmpty = true;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) 
-            {
-                if (board[i][j] == 0) 
-                {
-                    row = i;
-                    col = j;
-                    
-                    isEmpty = false;
-                    break;
-                }
-            }
-            if (!isEmpty) {
-                break;
-            }
-            if (isEmpty) {
-                return true;
-            }
-            //find an isSafe number
-            for (int num = 1; num <= n; num++ ){ 
-                if (isSafe(board, row, col, num)){
-                    board [row][col] = num;
-                    if (solve(board, n)){
-                        return true;
+    private static boolean isValidPlacement (int[][] board, int row, int column, int number){
+        return isRowSafe(board, row, number) && 
+        isColumnSafe(board, column, number) && 
+        isSquareSafe(board, row, column, number);
+    }
+
+    private static boolean solveBoard(int[][] board){
+        //loop through grid
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int column = 0; column < GRID_SIZE; column++){
+                
+                if (board[row][column] == 0) {  //empty space = 0
+
+                    for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++){ //go through numbers 1-9
+
+                        if (isValidPlacement(board, row, column, numberToTry)){ //is current number valid to place
+                            board [row][column] = numberToTry; //place it
+                            
+                            if (solveBoard(board)){ //starts again from top
+                                return true; 
+                            }
+                            else { //unable to place valid number
+                                board[row][column] = 0; //set numberToTry back to 0
+
+                            }
+                        }
                     }
-                }
-                //set back if no isSafe number found
-                else {
-                    board [row] [col] = 0;
+                    return false; 
                 }
             }
         }
-        return false;
+        return true;
+    }
+
+    private static void printSolvedBoard(int[][] board){
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row != 0) { //every third row
+                System.out.println("-----------");
+            }
+            for(int column = 0; column < GRID_SIZE; column++){
+                if (column % 3 == 0 && column != 0){ //every third column
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printBoard(int[][] board){
+        System.out.println("original board:");
+        System.out.println();
+        printSolvedBoard(board);
+        System.out.println();
+
+        if(solveBoard(board)){
+            System.out.println("solved board:");
+            System.out.println();
+        }
+        else {
+            System.out.println("unable to solve board");
+        }
+        printSolvedBoard(board);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int sudokuEasy [][] = { {0,4,0,0,0,7,0,0,0},    //0  !! remember row/column starts with 0
+                                {0,0,1,8,9,4,0,0,0},    //1
+                                {8,2,7,0,6,3,0,9,0},    //2
+                                {0,0,0,7,0,8,0,0,4},    //3
+                                {0,8,5,0,4,9,6,0,7},
+                                {0,7,0,6,5,1,2,8,0},
+                                {0,9,6,3,8,2,5,0,0},
+                                {7,0,0,9,1,0,0,0,0},
+                                {0,0,0,0,0,5,0,0,3}};
+        
+        int sudokuEvil [][] = { {0,7,0,2,0,0,0,8,0},
+                                {0,0,0,0,1,6,0,5,7},
+                                {0,8,6,7,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,2,0},
+                                {0,0,4,6,0,7,8,0,0},
+                                {0,9,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,2,7,3,0},
+                                {2,4,0,3,8,0,0,0,0},
+                                {0,6,0,0,0,5,0,9,0}};
+        
+        printBoard(sudokuEasy);
+        printBoard(sudokuEvil);
     }
 }
-
-
-
--- Testcode.java
+++ Testcode.java
@@ -1,16 +1,32 @@
 public class Testcode {
+    
+
     public static void main(String[] args) {
         //safety
-        int sudokuEasy [][] = { {0,4,0,0,0,7,0,0,0},
-                                {0,0,1,8,9,4,0,0,0},
-                                {8,2,7,0,6,3,0,9,0},
-                                {0,0,0,7,0,8,0,0,4},
+        int sudokuEasy [][] = { {0,4,0,0,0,7,0,0,0},    //0  !! remember row/column starts with 0
+                                {0,0,1,8,9,4,0,0,0},    //1
+                                {8,2,7,0,6,3,0,9,0},    //2
+                                {0,0,0,7,0,8,0,0,4},    //3
                                 {0,8,5,0,4,9,6,0,7},
                                 {0,7,0,6,5,1,2,8,0},
                                 {0,9,6,3,8,2,5,0,0},
                                 {7,0,0,9,1,0,0,0,0},
                                 {0,0,0,0,0,5,0,0,3}};
-        System.out.println(SudokuSolver.isSafe(sudokuEasy, 7, 5, 6)); //!! remember row/col starts with 0
-        System.out.println(SudokuSolver.isSafe(sudokuEasy, 0, 0, 3));
+        
+        System.out.println("true:" + SudokuSolver.isRowSafe(sudokuEasy, 0, 3));
+        System.out.println("false:" + SudokuSolver.isRowSafe(sudokuEasy, 0, 7));
+
+        System.out.println("true:" + SudokuSolver.isColumnSafe(sudokuEasy, 0, 3));
+        System.out.println("false:" + SudokuSolver.isColumnSafe(sudokuEasy, 0, 7));
+
+        System.out.println("true:" + SudokuSolver.isSquareSafe(sudokuEasy, 0, 0, 3));
+        System.out.println("false:" + SudokuSolver.isSquareSafe(sudokuEasy, 0, 0, 7));
+
+        System.out.println("true:" + SudokuSolver.isValidPlacement(sudokuEasy, 0, 0,3));
+        System.out.println("false:" + SudokuSolver.isValidPlacement(sudokuEasy, 0, 0,7));
+        System.out.println("false:" + SudokuSolver.isValidPlacement(sudokuEasy, 0, 0,1));
+
+        
+
     }
 }
