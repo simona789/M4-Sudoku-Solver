27ae472818b42458229775e08a7ed207f58a6e2b
Simona Ruoss <simonaruoss@Simonas-MacBook-Air.local>
2025-01-22 08:12:18 +0100

    Sudoku Generater added

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,72 +1,27 @@
 public class SudokuSolver {
 
-    private static final int GRID_SIZE = 9;
-
-    //check row
-    private static boolean isRowSafe(int[][] board, int row, int number){
-        for (int i = 0; i < GRID_SIZE; i++){
-            if (board[row][i] == number) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //check column
-    private static boolean isColumnSafe(int[][] board, int column, int number){
-        for (int i = 0; i < GRID_SIZE; i++){
-            if (board[i][column] == number) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //check square
-    private static boolean isSquareSafe(int[][] board, int row, int column, int number){
-        //get top left box of square
-        int localSquareRow = row - row % 3;
-        int localSquareColumn = column - column % 3;
-        //go through rows
-        for (int i = localSquareRow; i < localSquareRow + 3; i ++){
-            //go through columns
-            for (int j = localSquareColumn; j < localSquareColumn + 3; j++){
-                if (board [i][j] == number){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean isValidPlacement (int[][] board, int row, int column, int number){
-        return isRowSafe(board, row, number) && 
-        isColumnSafe(board, column, number) && 
-        isSquareSafe(board, row, column, number);
-    }
-
     private static boolean solveBoard(int[][] board){
         //loop through grid
-        for (int row = 0; row < GRID_SIZE; row++) {
-            for (int column = 0; column < GRID_SIZE; column++){
+        for (int row = 0; row < Sudoku.GRID_SIZE; row++) {
+            for (int column = 0; column < Sudoku.GRID_SIZE; column++){
                 
-                if (board[row][column] == 0) {  //empty space = 0
+                if (board[row][column] == 0) {  //space = 0
 
-                    for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++){ //go through numbers 1-9
+                    for(int numberToTry = 1; numberToTry <= Sudoku.GRID_SIZE; numberToTry++){ //go through numbers 1-9
 
-                        if (isValidPlacement(board, row, column, numberToTry)){ //is current number valid to place
+                        if (Sudoku.isValidPlacement(board, row, column, numberToTry)){ //current number is valid to place
                             board [row][column] = numberToTry; //place it
                             
                             if (solveBoard(board)){ //starts again from top
                                 return true; 
                             }
-                            else { //unable to place valid number
+                            else { //unable to place a valid number
                                 board[row][column] = 0; //set numberToTry back to 0
 
                             }
                         }
                     }
-                    return false; 
+                    return false; //unable to solve board
                 }
             }
         }
@@ -74,11 +29,11 @@ public class SudokuSolver {
     }
 
     private static void printSolvedBoard(int[][] board){
-        for (int row = 0; row < GRID_SIZE; row++) {
+        for (int row = 0; row < Sudoku.GRID_SIZE; row++) {
             if (row % 3 == 0 && row != 0) { //every third row
                 System.out.println("-----------");
             }
-            for(int column = 0; column < GRID_SIZE; column++){
+            for(int column = 0; column < Sudoku.GRID_SIZE; column++){
                 if (column % 3 == 0 && column != 0){ //every third column
                     System.out.print("|");
                 }
@@ -125,8 +80,41 @@ public class SudokuSolver {
                                 {0,0,0,0,0,2,7,3,0},
                                 {2,4,0,3,8,0,0,0,0},
                                 {0,6,0,0,0,5,0,9,0}};
+                        
+        int sudokuGenerated [][] = {{1,7,0,2,3,4,0,9,5},
+                                    {2,9,5,6,7,1,4,3,0}, 
+                                    {0,6,3,5,8,9,1,0,2}, 
+                                    {7,1,0,0,5,6,0,8,0}, 
+                                    {3,2,4,1,9,8,0,0,6},
+                                    {0,8,6,4,0,7,0,1,0}, 
+                                    {8,4,2,7,1,0,5,6,0},
+                                    {6,0,1,9,4,5,8,2,7},
+                                    {9,5,7,0,6,2,3,0,1}};
         
+        int sudokuGenerated50 [][] = {{6,0,0,1,2,0,0,0,0},
+            {0,0,9,0,3,0,1,8,4},
+            {7,0,4,0,0,0,0,6,0},
+            {1,0,0,0,8,0,0,0,0},
+            {4,0,0,7,1,5,3,0,0},
+            {0,0,7,0,4,0,0,0,0},
+            {0,0,0,0,0,1,9,0,7},
+            {0,0,1,0,0,0,6,4,8},
+            {0,0,6,8,0,0,0,2,1}};
+
+        int sudokuGenerated80 [][] = {{0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {6,0,0,0,0,0,0,0,0}};
+
+        
+        printBoard(sudokuGenerated);
         printBoard(sudokuEasy);
         printBoard(sudokuEvil);
+        
     }
 }
